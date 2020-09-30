2dda051e01fda5741d8facb195a18a1263516fca
siyabonga <growthstop@gmail.com>
2020-10-01 00:12:10 +0200

    project completed

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -43,4 +43,10 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+
+    public void createPhoneIntent(View view){
+        Intent PhoneIntent = new Intent(Intent.ACTION_DIAL);
+        PhoneIntent.setData(Uri.parse("tel:0123456789"));
+        startActivity(PhoneIntent);
+    }
 }
